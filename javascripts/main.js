099fbf6cde88f2d49c61c2c48100ccaaca8cd824
Tizik Pauline <43169531+tizikpauline@users.noreply.github.com>
2019-03-26 15:27:57 +0300

    new commit

--- javascripts/main.js
+++ javascripts/main.js
@@ -218,7 +218,7 @@ $(document).ready(function(){
     event.preventDefault();
     var id  = $(this).attr('href'),
         top = $(id).offset().top;
-    $('body,html').animate({scrollTop: top}, 2000);
+    $('body,html').animate({scrollTop: top}, 1500);
   });
 });
 
